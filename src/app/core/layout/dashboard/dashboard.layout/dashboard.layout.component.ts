3cce4af1d004de8fcc5bd75acfcec8d0ef40eeba
eliezer-chaves <chaves.eliezer@outlook.com>
2025-05-28 11:37:10 -0300

    💄: implement responsive dashboard layout with collapsible sidebar

--- src/app/core/layout/dashboard/dashboard.layout/dashboard.layout.component.ts
+++ src/app/core/layout/dashboard/dashboard.layout/dashboard.layout.component.ts
@@ -1,11 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 @Component({
   selector: 'app-dashboard.layout',
   templateUrl: './dashboard.layout.component.html',
   styleUrls: ['./dashboard.layout.component.css'],
-  imports: [RouterModule],
+  imports: [RouterModule, NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule, NgClass],
 
 })
-export class DashboardLayoutComponent  {}
+export class DashboardLayoutComponent {
+  isCollapsed = true;
+
+  toggleSider(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+}
